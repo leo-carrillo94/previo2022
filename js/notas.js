c251101c33817b09bf3f5e093466639066bb04ef
Leonel Alberto Carrillo Contreras <leonelalbertocc@ufps.edu.co>
2022-04-18 12:16:13 -0500

    final

--- js/notas.js
+++ js/notas.js
@@ -3,12 +3,17 @@ const $template = document.getElementById("datosDinamicos").content,
 $datos = document.querySelector(".datos"),
 $fragment = document.createDocumentFragment();
 
+var codigo = localStorage.getItem("codigo");
+var nombre = localStorage.getItem("nombre");
+
+document.getElementById("codigo").innerHTML = codigo;
+document.getElementById("nombre").innerHTML = nombre;
+console.log(codigo);
 
 
 
 
-
-var url = 'https://api-parcial.crangarita.repl.co/notas';
+var url = `https://api-parcial.crangarita.repl.co/students/${codigo}/notas`;
 
 
 fetch(url, {
@@ -42,5 +47,10 @@ $datos.querySelector("tbody").appendChild($fragment);
 
 }
 );
+
+document.getElementById("salir").onclick() = function{
+    localStorage.removeItem("codigo");
+    localStorage.removeItem("nombre");
+}
 
 
